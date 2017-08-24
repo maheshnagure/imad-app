7b30b6105c942893702d2653572a21c41cdcc0dc
maheshnagure <31087730+maheshnagure@users.noreply.github.com>
2017-08-24 18:12:54 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.get('/article-three', function(req, res) {
     res.send("Article Three requested and will be server here");
 });
 
-//app.get('/ui/style.css', function (req, res) {
-//  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-//});
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
 //app.get('/ui/madi.png', function (req, res) {
 //  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
