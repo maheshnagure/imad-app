703e1ce849951bed081fb3d67f8b05fe9e6027b5
maheshnagure <31087730+maheshnagure@users.noreply.github.com>
2017-08-24 13:22:58 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.get('/article-three', function(req, res) {
     res.send("Article Three requested and will be server here");
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+//app.get('/ui/style.css', function (req, res) {
+//  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+//});
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+//app.get('/ui/madi.png', function (req, res) {
+//  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+//});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
